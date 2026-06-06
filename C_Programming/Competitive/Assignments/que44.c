e137292eb3fa01b930eb809907ea79e3a664ddc7
Rutuja-developer <rutujamk2004@gmail.com>
2026-06-06 22:32:40 +0530

    Add files via upload

---
+++ C_Programming/Competitive/Assignments/que44.c
@@ -0,0 +1,34 @@
+//Accept number from user and return multiplication of all digit.
+// Input : 2395
+//Output : 270
+
+#include<stdio.h>
+int MultDigit(int iNo)
+{
+    int iDigit = 0;
+    int iMult = 1;
+
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+        iMult = iMult * iDigit;
+        iNo = iNo / 10;
+         
+    }
+   
+    return iMult;
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("enter number:");
+    scanf("%d",&iValue);
+
+    iRet = MultDigit(iValue);
+
+    printf("%d",iRet);
+    
+    return 0;
+}
